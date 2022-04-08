e7ebedc92daac9f3f660da41680d824cf42816bf
Abdu-m <83346757+Abdu-m@users.noreplay.github.com>
2022-04-08 02:58:05 +0000

    print to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  *main - Entry point
- *REturn: 1 if success
+ *REturn: return 1 if success
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n\n", 58);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
