0fc778df147d76aa3ffd8f1a43408fc142308270
Abdu-m <83346757+Abdu-m@users.noreplay.github.com>
2022-04-08 21:09:55 +0000

    print base 16 number in lower case

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - print hexadecimal
+ *Return: 0 (success)
+*/
+int main(void)
+{
+char hex;
+for (hex = 48; hex <= 57; hex++)
+putchar(hex);
+for (hex = 97; hex <= 102; hex++)
+putchar(hex);
+putchar('\n');
+return (0);
+}
