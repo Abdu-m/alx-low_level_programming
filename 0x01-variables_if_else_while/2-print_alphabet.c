92bf73cdb5d7e98d82028b5c9be06f2bfc209e5f
Abdu-m <83346757+Abdu-m@users.noreplay.github.com>
2022-04-08 14:05:22 +0000

    print alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - alphabet
+ *
+ *Return: always 0 (succes) 
+ *
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= '2'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
