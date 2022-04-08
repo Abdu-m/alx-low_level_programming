d432dab98f928156605c5713bf545c0f703a3349
Abdu-m <83346757+Abdu-m@users.noreplay.github.com>
2022-04-08 00:31:30 +0000

    printing by using printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  *main -Entry point
- *print using printf function
+ *print using printf
  *Return: 0 after program execution
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf ("with proper grammar, but the outcome is a piece of art,\n");
 return(0);
 }
