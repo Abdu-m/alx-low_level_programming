d3500cba3696209d76837d290db103b17c8df04c
Abdu-m <83346757+Abdu-m@users.noreplay.github.com>
2022-04-12 20:41:47 +0000

    check for sign of numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_sign - diplay sign of numbers
+*Return: 0 (success)
+*/
+int print_sign(int n)
+{
+int n;
+if (n = 0)
+putchar('0');
+else if (n < 0)
+putchar('-');
+else
+putchar('+');
+return (0);
+}
