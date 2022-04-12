50bea52a333d3bfee9bcab48582fd23ca0b6ae9c
Abdu-m <83346757+Abdu-m@users.noreplay.github.com>
2022-04-12 22:13:04 +0000

    print every natural number from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*print_to_98 - print all natral number from n to 98
+*@n: takes an integer
+*/
+void print_to_98(int n)
+{
+int i;
+	if (n > 98)
+	{
+		for (i = n; i >97; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			printf(", ");
+		}
+	}
+	else
+	{
+		for (i = n; i < 99; i++)
+		{
+			printf("%d", i);
+			if(i != 98)
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
