226da1e9004d7074c651718d0e1a42b31e64f654
Abdu-m <83346757+Abdu-m@users.noreplay.github.com>
2022-04-12 19:52:31 +0000

    return 1 if its lowercase otherwise return 0

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*_islower - return 1 if itis lower case or 0 otherwise
+*Return: 0 (upper case) , 1 (lower case)
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	return (1);
+	else
+	return (0);
+}
