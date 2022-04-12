cf2e7e531c3aa1ec2a8291306b6ad5c2ce133558
Abdu-m <83346757+Abdu-m@users.noreplay.github.com>
2022-04-12 19:09:55 +0000

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
+#include "main.h"
 /**
  *main - entery point
  *Return: 0
 */
 int main(void)
 {
-printf("_putchar\n");
+	char c = "_puchar";
+	_putchar(c);
+	_putchar("\n");
 return (0);
 }
