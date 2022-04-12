7a3808417386b5ab56e10e5b3a2ab0d4a5a86bc4
Abdu-m <83346757+Abdu-m@users.noreplay.github.com>
2022-04-12 22:42:03 +0000

    print sign of number and return 0, 1, -1

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,10 +6,19 @@
 */
 int print_sign(int n)
 {
-if (n > 0)
-putchar('+');
-else if (n < 0)
-putchar('-');
-else
-putchar('0');
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+	else
+	{
+		putchar('0');
+		return (0);
+	}
 }
