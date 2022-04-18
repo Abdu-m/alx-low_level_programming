8fb5006eb8b9f5b07f78ca4c6e50520a48cf6915
Abdu-m <83346757+Abdu-m@users.noreplay.github.com>
2022-04-18 22:09:10 +0000

    function that print a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,14 +6,13 @@
 */
 void print_rev(char *s)
 {
-	int i, n;
+	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		continue;
 	}
-	n = i - 1;
-	for (i = n; i >= 0; i++)
+	for (i = i - 1; i >= 0; i--)
 	{
 	_putchar(s[i]);
 	}
