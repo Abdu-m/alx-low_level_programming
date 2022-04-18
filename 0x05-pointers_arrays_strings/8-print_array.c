64b2443ea5377ab2558a3eaeabe7597af1d72b71
Abdu-m <83346757+Abdu-m@users.noreplay.github.com>
2022-04-18 23:14:02 +0000

    function that print n element of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,19 +2,21 @@
 #include <stdio.h>
 /**
  * print_array - print n element of an array
- * @a; pointer 
- * @n; return value
+ * @a: pointer 
+ * @n: return value
  * Return: void
 */
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < 0; i++)
+	i = 0;
+	while (i < 0)
 	{
 		printf("%d", a[i]);
 		if (i < n - 1)
 		printf(", ");
+		i++;
 	}
 	printf("\n");
 }
