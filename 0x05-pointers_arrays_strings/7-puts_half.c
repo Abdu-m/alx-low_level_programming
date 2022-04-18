4f8deb60ca3cca7a81b9dc63ea1b3e187ea945df
Abdu-m <83346757+Abdu-m@users.noreplay.github.com>
2022-04-18 22:51:44 +0000

    function print last half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - print half of string
+ * @str: pointer
+ * Return: void
+*/
+void puts_half(char *str)
+{
+	int i, last;
+
+	for ( i = 0; str[i] != '\0'; i++)
+	{
+		continue;
+	}
+	last + (i + 1) / 2;
+	for (i = last; str[i]; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
