a92e9a89de1dbbd0f6ca5cf076009052c8267dd1
Abdu-m <83346757+Abdu-m@users.noreplay.github.com>
2022-04-18 22:36:29 +0000

    function that prints every char of string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - print every char of a string
+ * @str: pointer
+ * Return: void
+*/
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\n'; i++)
+	{
+		if (i % 2 == 0)
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
