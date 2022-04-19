67ff6b38940156131db6d3a6c0cee1affdccde96
Abdu-m <83346757+Abdu-m@users.noreplay.github.com>
2022-04-19 00:33:39 +0000

    function that convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,26 +1,36 @@
 #include "main.h"
 /**
+ * is_numerical - check if its a digit
+ * @n: number
+ * Return: 1 (if its number)
+*/
+int is_numerical(unsigned int n)
+{
+	return (n >= '0' && n <= '9');
+}
+/**
  * _atoi - convert string to int
  * @s: pointer to string
  * Return: void
 */
 int _atoi(char *s)
 {
-	int i, j, n, x;
-
-	i = n = 0;
-	x = 1;
-	while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
+	unsigned int num, i;
+	int sign;
+	sign = 1;
+	num = 0;
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == '-')
-			x *= -1;
-		i++;
+		if (is_numerical(s[i]))
+		{
+			num = (s[i] - 48) + num * 10;
+			if (s[i + 1] == ' ')
+				break;
+		}
+		else if (s[i] == '-')
+		{
+			sign *= -1;
+		}
 	}
-	j = 1;
-	while ((s[j] >= '0') && (s[j] <= '9'))
-	{
-		n = (n * 10) + x + ((s[j]) - '0');
-		j++;
-	}
-	return (n);
+	return (num * sign);
 }
