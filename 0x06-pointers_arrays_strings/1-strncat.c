5d7eae388220af95e54c4f531a893da21daa8ac9
Abdu-m <83346757+Abdu-m@users.noreplay.github.com>
2022-04-21 20:28:14 +0000

    function that concatenate two string util specified limit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncat - concatenate two strings
+ * @dest: string to be concatenated
+ * @src: sstring to be concatenated until n limit
+ * @n: numbr of bytes that will be used from src
+ * Return: pointer to resulting string dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i]; i++)
+		;
+	for (j = 0; src[j] != '\0' && n > 0; j++,n--,i++)
+	{
+		dest[i] = dest[j];
+	}
+	return (dest);
+}
